ba82474337d1522cc7ffd80bb5ed681cdd1f9a80
Michael Jones <mtjones1979@yahoo.com>
2021-03-28 15:14:27 -0500

    updated category routes for ex in activities 5 and 7

--- models/index.js
+++ models/index.js
@@ -5,9 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 // Product belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
